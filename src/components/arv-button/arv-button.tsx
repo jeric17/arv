a6ffcb87ed509359a332b60a85c1adaaef144bbf
mespiritu <marcespiritu17@gmail.com>
2018-08-27 16:12:30 +0800

    add button component.

---
+++ src/components/arv-button/arv-button.tsx
@@ -0,0 +1,52 @@
+import { Component, Prop } from '@stencil/core';
+import cx from 'classnames';
+
+@Component({
+  tag: 'arv-button',
+  styleUrl: 'arv-button.css',
+  shadow: true
+})
+export class Button {
+
+  /* name: color
+   * oneOf: [default, primary, secondary, inherit] */
+  @Prop() color: string = 'default';
+
+  /* name: disabled
+   * type: boolean */
+  @Prop() disabled: boolean = false;
+
+  /* name: full
+   * type: boolean */
+  @Prop() full: boolean = false;
+
+  /* name: size
+   * oneOf: [small, medium, large] */
+  @Prop() size: string = 'medium';
+
+  /* name: variant
+   * oneOf: [flat, raised] */
+  @Prop() variant: string = 'flat';
+
+  render() {
+    const rootClassNames = cx('arv-button', {
+      'default': this.color === 'default',
+      inherit: this.color === 'inherit',
+      primary: this.color === 'primary',
+      secondary: this.color === 'secondary',
+      disabled: this.disabled,
+      full: this.full,
+      small: this.size === 'small',
+      medium: this.size === 'medium',
+      large: this.size === 'large',
+      flat: this.variant === 'flat',
+      raised: this.variant === 'raised',
+    });
+
+    return (
+      <button class={rootClassNames}>
+        <slot></slot>
+      </button>
+    );
+  }
+}
--- src/components/arv-flex/arv-flex.tsx
+++ src/components/arv-flex/arv-flex.tsx
@@ -7,28 +7,28 @@ import { Component, Prop } from '@stencil/core';
 })
 export class Flex {
 
-  /* name: layout
-   * oneOf: [row, row-reverse, column, column-reverse] */
-  @Prop() layout: string = 'row';
-
-  /* name: wrap
-   * oneOf: [wrap, no-wrap, wrap-reverse] */
-  @Prop() wrap: string = 'wrap';
-
-  /* name: justify
-   * oneOf: [center, start, end, around, between, evenly] */
-  @Prop() justify: string = 'start';
+  /* name: content
+   * oneOf: [start, end, center, stretch, around, between] */
+  @Prop() content: string = 'start';
 
   /* name: items
    * oneOf: [start, end, center, stretch, baseline] */
   @Prop() items: string = 'start';
 
-  /* name: content
-   * oneOf: [start, end, center, stretch, around, between] */
-  @Prop() content: string = 'start';
+  /* name: justify
+   * oneOf: [center, start, end, around, between, evenly] */
+  @Prop() justify: string = 'start';
+
+  /* name: layout
+   * oneOf: [row, row-reverse, column, column-reverse] */
+  @Prop() layout: string = 'row';
 
   /* type: number */
   @Prop() order: number = 0;
+
+  /* name: wrap
+   * oneOf: [wrap, no-wrap, wrap-reverse] */
+  @Prop() wrap: string = 'wrap';
 
   render() {
     const rootClassNames = `arv-flex layout-${this.layout} justify-${this.justify} items-${this.items} content-${this.content}`;
