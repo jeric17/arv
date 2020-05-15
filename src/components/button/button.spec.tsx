39e9374166875c2bed7084e194ed143d7716098d
marcespiritu <lespiritu@vlocity.com>
2020-05-15 16:10:13 +0800

    refactor button, greatly decreased build file size. removed excess codes.

---
+++ src/components/button/button.spec.tsx
@@ -0,0 +1,54 @@
+import { Button } from './button';
+import { createSpec, getShadowEl } from '../../utils/testing/utils';
+
+const specComponent = createSpec(Button);
+
+it('render the component', async () => {
+  const page = await specComponent(`
+    <arv-button>Button</arv-button>
+  `);
+
+  expect(page.root.shadowRoot).toBeTruthy();
+});
+
+it('set color', async () => {
+  const page = await specComponent(`
+    <arv-button color="primary"></arv-button>
+  `);
+  expect(getShadowEl(page, 'button').classList.contains('primary')).toBeTruthy();
+});
+
+it('set size', async () => {
+  const page = await specComponent(`
+    <arv-button size="large"></arv-button>
+  `);
+  expect(getShadowEl(page, 'button').classList.contains('large')).toBeTruthy();
+});
+
+it('set variant', async () => {
+  const page = await specComponent(`
+  <arv-button variant="raised"></arv-button>
+`);
+  expect(getShadowEl(page, 'button').classList.contains('raised')).toBeTruthy();
+});
+
+it('set full', async () => {
+  const page = await specComponent(`
+  <arv-button full></arv-button>
+`);
+  expect(page.root.classList.contains('full')).toBeTruthy();
+});
+
+it('set disabled', async () => {
+  const page = await specComponent(`
+  <arv-button disabled></arv-button>
+`);
+  expect(getShadowEl(page, 'button').hasAttribute('disabled')).toBeTruthy();
+});
+
+it('set boxed', async () => {
+  const page = await specComponent(`
+  <arv-button boxed></arv-button>
+`);
+  expect(getShadowEl(page, 'button').classList.contains('boxed')).toBeTruthy();
+});
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,168 +1,88 @@
-import { Component, h, Event, EventEmitter, Prop } from '@stencil/core';
+import { Component, Prop, Host, h } from '@stencil/core';
+import { Color, Size } from '../../interface';
+import { generateAttrValue } from '../../utils/helpers';
 
+/**
+ * @slot - Content or middle content of the button.
+ * @slot start - Left content of the button.
+ * @slot end - Right content of the button.*
+ */
 @Component({
   tag: 'arv-button',
   styleUrl: 'button.css',
-  shadow: true
+  shadow: true,
 })
 export class Button {
 
-  /* oneOf: [default, primary, secondary, inherit] */
-  @Prop() color: string = 'default';
-  @Prop() disabled: boolean = false;
-  @Prop() full: boolean = false;
-  @Prop() href: string;
-  @Prop() icon: string;
-  @Prop() buttonClick: (e: MouseEvent) => void;
-  @Prop() layout: string;
-  @Prop() loading: boolean;
-  @Prop() padded = true;
-  @Prop() rounded: boolean = true;
-  /* oneOf: [small, medium, large] */
-  @Prop() size: string = 'medium';
-  @Prop() styles: any;
-  /* oneOf: [start, center, end]*/
-  @Prop() textAlign: string = 'center';
-  @Prop() type: string;
-  /* oneOf: [bordered, flat, raised, flat-icon, raised-icon] */
-  @Prop() variant: string = 'flat';
-  /* oneOf: [dialogOk, dialogCancel, fileUpload] */
-  @Prop() roleType: string;
-  @Prop() multipleFileUpload: boolean;
+  /**
+   * Color variant to use.
+   */
+  @Prop() color?: Color;
 
-  @Event() arvButtonClick: EventEmitter;
-  @Event() arvButtonFileUpload: EventEmitter;
+  /**
+   * Size variant to use.
+   */
+  @Prop() size?: Size;
 
-  hostData() {
-    return {
-      class: {
-        full: this.full
-      }
-    };
-  }
+  /**
+   * Button variant to use.
+   */
+  @Prop() variant?: 'raised' | 'ghost' | string;
 
-  btnClick(e: MouseEvent) {
-    if (this.disabled || this.loading) {
-      return false;
-    }
-    this.arvButtonClick.emit({
-      event: e,
-      type: this.type,
-      roleType: this.roleType
-    });
+  /**
+   * Fullwidth button.
+   */
+  @Prop() full?: boolean;
 
-    return this.buttonClick && this.buttonClick(e);
-  }
+  /**
+   * Disable the button.
+   */
+  @Prop() disabled?: boolean;
 
-  onFileChange = (event: any) => {
-    this.arvButtonFileUpload.emit(event.target.files);
-  }
+  /**
+   * Edged corners.
+   */
+  @Prop() boxed?: boolean;
+
+  /**
+   * Css styles to extend the component's ui
+   */
+  @Prop() styles?: { [key: string]: string };
+
+  /**
+   * Deprecated props
+   */
+  @Prop() textAlign?: any;
+  @Prop() buttonClick?: any;
+  @Prop() icon?: any;
+  @Prop() rounded?: any;
+  @Prop() padded?: any;
 
   render() {
-    const rootClassNames = {
-      button: true,
-      'default': this.color === 'default',
-      inherit: this.color === 'inherit',
-      primary: this.color === 'primary',
-      secondary: this.color === 'secondary',
-      warning: this.color === 'warning',
-      light: this.color === 'light',
-      dark: this.color === 'dark',
-      error: this.color === 'error',
-      success: this.color === 'success',
-      disabled: this.disabled === true,
-      full: this.full === true,
-      small: this.size === 'small',
-      medium: this.size === 'medium',
-      large: this.size === 'large',
-      xlarge: this.size === 'xlarge',
-      bordered: this.variant === 'bordered',
-      flat: this.variant === 'flat',
-      raised: this.variant === 'raised',
-      flatIcon: this.variant === 'flat-icon',
-      raisedIcon: this.variant === 'raised-icon',
-      fab: this.variant === 'fab',
-      ghost: this.variant === 'ghost',
-      ghostIcon: this.variant === 'ghost-icon',
-      boxed: !this.rounded,
-      noPad: !this.padded,
-      loading: this.loading
+    const hostCls = {
+      full: this.full
     };
 
-    const iconMode = ['fab', 'flat-icon', 'raised-icon'];
-
-    const Icon = () => (
-      <arv-icon
-        noMargin={rootClassNames.flatIcon || rootClassNames.raisedIcon || rootClassNames.fab}
-        withButtonIcon={this.icon && !iconMode.includes(this.variant)}
-        size={this.size}
-        icon={this.icon}
-      >
-      </arv-icon>
-    );
-
-    const C = () => (
-      <arv-flex
-        layout={this.layout}
-        items="center"
-        justify={this.textAlign}>
-        {this.icon && <Icon />}
-        {(this.variant !== 'fab') && (
-          <div class="slot">
-            <arv-text>
-              <slot></slot>
-            </arv-text>
-          </div>
-        )}
-      </arv-flex>
-    );
-
-    const Loader = () => (
-      <arv-flex items="center" justify="center">
-        <arv-loader size="xsmall"></arv-loader>
-      </arv-flex>
-    );
-
-    const T = ({ p }) => {
-      if (this.href) {
-        return <a href={this.href} {...p}><C /></a>
-      }
-      if (this.roleType === 'fileUpload') {
-        return (
-          <div class="uploadWrapper">
-            <label class="uploadLabel">
-              <a {...p}>
-                <C />
-              </a>
-              <input
-                disabled={this.disabled}
-                id="file"
-                type="file"
-                multiple={this.multipleFileUpload}
-                onChange={this.onFileChange}
-                class="input"
-              />
-            </label>
-          </div>
-        );
-      }
-      return (
-        <button {...p}>
-          {!this.loading && <C />}
-          {this.loading && <Loader />}
-        </button>
-      );
-    };
-
-    const props = {
-      style: this.styles,
-      class: rootClassNames,
-      type: this.type,
-      onClick: this.btnClick.bind(this)
+    const rootCls = {
+      ...generateAttrValue(this.color),
+      ...generateAttrValue(this.size),
+      ...generateAttrValue(this.variant),
+      boxed: this.boxed,
     };
 
     return (
-      <T p={props}></T>
+      <Host class={hostCls}>
+        <button
+          style={this.styles}
+          class={rootCls}
+          disabled={this.disabled}>
+          <span class="content">
+            <slot name="start"></slot>
+            <slot></slot>
+            <slot name="end"></slot>
+          </span>
+        </button>
+      </Host>
     );
   }
 }
